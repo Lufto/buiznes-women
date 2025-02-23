347c48bd6a521f02f31d36db6f4ca4cd463ecaa0
Lufto <sashatsar11@mail.ru>
2025-02-23 09:48:02 +0400

    env

--- server/js/middleware.js
+++ server/js/middleware.js
@@ -1,10 +1,11 @@
-import express from 'express'
-import cors from 'cors'
 import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
 
 const middleware = express.Router()
 
 middleware.use(cors())
+middleware.options('*', cors())
 middleware.use(bodyParser.urlencoded({ extended: false }))
 middleware.use(bodyParser.json())
 
